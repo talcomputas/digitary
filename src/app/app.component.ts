26ed072586fd3003044ab224dc58ed02e342e351
Trond Albinussen <tal@computas.com>
2020-06-25 10:42:35 +0200

    fingers crossed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,5 +5,5 @@ import { Component } from '@angular/core';
   template: `<div>Hello {{value}}</div>`,
 })
 export class AppComponent {
-  value = 'digitary app is running!';
+  value = 'build from github';
 }
